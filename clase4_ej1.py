c97adc08df44298929f51e5f50e05762077e014c
Matias Fondini <mfondini@natconsultores.com.ar>
2023-08-31 20:51:48 -0300

    Modificacion de ejercicio 1 con for loop

--- clase4_ej1.py
+++ clase4_ej1.py
@@ -1,11 +1,7 @@
 list=[]
-num1 = int(input("Ingrese un numero: "))
-num2 = int(input("Ingrese un numero: "))
-num3 = int(input("Ingrese un numero: "))
-num4 = int(input ("Ingrese un numero: "))
-num5 = int(input("Ingrese un numero: "))
-list.extend([num1, num2, num3, num4, num5])
-
+for i in range(5):
+    num = int(input("Ingrese un numero"))
+    list.append(num)
 list.sort()
 print("Max: ",list[0])
 print("Min: ",list[4])
